dd8ec77d80aaee90277f695e6471be86010343c5
sarath <sarathmm224@gmail.com>
2023-11-08 23:39:56 +0530

    GREEN:fix test_count_neighbors function

--- game_of_life.py
+++ game_of_life.py
@@ -13,4 +13,15 @@ def print_grid(grid):
     for row in grid:
         print(" ".join([alive if cell else dead for cell in row]))
 
+def count_neighbors(grid, x, y):
+    rows, cols = len(grid), len(grid[0])
+    neighbors = [(x-1, y-1), (x-1, y), (x-1, y+1),
+                (x, y-1),               (x, y+1),
+                (x+1, y-1), (x+1, y), (x+1, y+1)]
+    count = 0
+    for nx, ny in neighbors:
+        if 0 <= nx < rows and 0 <= ny < cols and grid[nx][ny] == 1:
+            count += 1
+    return count
+
 
