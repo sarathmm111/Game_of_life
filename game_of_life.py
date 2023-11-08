7fd67fcb273eb449a2a3689502e4a4b1b264a44c
sarath <sarathmm224@gmail.com>
2023-11-08 23:31:41 +0530

    fix test_game_of_life function

---
+++ game_of_life.py
@@ -0,0 +1,10 @@
+import random
+
+def create_grid(rows, cols):
+    grid = []
+    for _ in range(rows):
+        row = [random.randint(0, 1) for _ in range(cols)]
+        grid.append(row)
+    return grid
+
+
